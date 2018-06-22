7c63aa9e8e9873822c0098c859f1cd1b7577dd4c
zwilder <zacharywilder@rocketmail.com>
2018-06-21 18:52:57 -0700

    Merged upwards from master

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,3 +1,24 @@
+/*
+* Barbarian!
+* Copyright (C) Zach Wilder 2018
+* 
+* This file is a part of Barbarian!
+*
+* Barbarian! is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Barbarian! is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Barbarian!.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <iostream>
 #include "../include/texture.hpp"
 
