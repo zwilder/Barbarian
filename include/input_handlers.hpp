05a5d36e0987829278e3aa1d745c9b4c73f2570b
zwilder <zacharywilder@rocketmail.com>
2018-07-28 11:12:48 -0700

    Cleaned up Input class.

--- include/input_handlers.hpp
+++ include/input_handlers.hpp
@@ -18,55 +18,52 @@
 * along with Barbarian!.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #pragma once
 #ifndef INPUT_HANDLERS_HPP
 #define INPUT_HANDLERS_HPP
 
 #include "vector.hpp"
+#include "bitflag.hpp"
 
-// Bit flags are possibly not the best way to do this...
-enum Cmd : int
-{
-    NONE = 0,
-    MOVE = 0x002,
-    QUIT = 0x004,
-    FULLSCREEN = 0x008,
-    NEXT_LEVEL = 0x010,
-    ENTER = 0x020,
-    GET = 0x040,
-    OPEN_INV = 0x080,
-    OPEN_DROP = 0x100,
-    LOOK = 0x200,
-    ESCAPE = 0x400
-};
-        
-
-class Input
+class Input : public wsl::BitFlag
 {
     public:
         Input(int cmd = Cmd::NONE);
         Input(int cmd, wsl::Vector2i delta, char alpha = 0, int mod = 0);
 
-        bool move() { return((cmdMask_ & Cmd::MOVE) == Cmd::MOVE); }
-        bool quit() { return((cmdMask_ & Cmd::QUIT) == Cmd::QUIT); }
-        bool fullscreen() { return((cmdMask_ & Cmd::FULLSCREEN) == Cmd::FULLSCREEN); }
-        bool nextLevel() { return((cmdMask_ & Cmd::NEXT_LEVEL) == Cmd::NEXT_LEVEL); }
-        bool enter() { return((cmdMask_ & Cmd::ENTER) == Cmd::ENTER); }
-        bool get() { return((cmdMask_ & Cmd::GET) == Cmd::GET); }
-        bool openInv() { return((cmdMask_ & Cmd::OPEN_INV) == Cmd::OPEN_INV); }
-        bool openDrop() { return((cmdMask_ & Cmd::OPEN_DROP) == Cmd::OPEN_DROP); }
-        bool look() { return((cmdMask_ & Cmd::LOOK) == Cmd::LOOK); }
-        bool escape() { return((cmdMask_ & Cmd::ESCAPE) == Cmd::ESCAPE); }
+        enum Cmd : uint16_t
+        {
+            NONE = 0,
+            MOVE = 0x002,
+            QUIT = 0x004,
+            FULLSCREEN = 0x008,
+            NEXT_LEVEL = 0x010,
+            ENTER = 0x020,
+            GET = 0x040,
+            OPEN_INV = 0x080,
+            OPEN_DROP = 0x100,
+            LOOK = 0x200,
+            ESCAPE = 0x400
+        };
+            
+        bool move() { return(check(Cmd::MOVE)); }
+        bool quit() { return(check(Cmd::QUIT)); }
+        bool fullscreen() { return(check(Cmd::FULLSCREEN)); }
+        bool nextLevel() { return(check(Cmd::NEXT_LEVEL)); }
+        bool enter() { return(check(Cmd::ENTER)); }
+        bool get() { return(check(Cmd::GET)); }
+        bool openInv() { return(check(Cmd::OPEN_INV)); }
+        bool openDrop() { return(check(Cmd::OPEN_DROP)); }
+        bool look() { return(check(Cmd::LOOK)); }
+        bool escape() { return(check(Cmd::ESCAPE)); }
 
         wsl::Vector2i dir() { return dir_; }
         char alpha() { return alpha_; }
         int mod() { return mod_; }
     private:
-        int mod_;
         wsl::Vector2i dir_; 
         char alpha_;
-        int cmdMask_;
+        int mod_;
  };
 
 Input handleKeys(int key, int mod);
--- src/input_handlers.cpp
+++ src/input_handlers.cpp
@@ -23,16 +23,17 @@
 #include <SDL2/SDL.h>
 #include "../include/input_handlers.hpp"
 
-Input::Input(int cmd) : cmdMask_(cmd)
+Input::Input(int cmd)
 {
-   dir_ = wsl::Vector2i();
-   alpha_ = char(0);
-   mod_ = KMOD_NONE;
+    set(cmd);
+    dir_ = wsl::Vector2i();
+    alpha_ = char(0);
+    mod_ = KMOD_NONE;
 }
 
-Input::Input(int cmd, wsl::Vector2i delta, char alpha, int mod) : cmdMask_(cmd), dir_(delta), alpha_(alpha), mod_(mod)
+Input::Input(int cmd, wsl::Vector2i delta, char alpha, int mod) : dir_(delta), alpha_(alpha), mod_(mod)
 {
-    //
+    set(cmd);
 }
 
 Input handleKeys(int key, int mod)
@@ -40,135 +41,135 @@ Input handleKeys(int key, int mod)
     switch(key)
     {
 
-        case SDLK_a: return Input(Cmd::NONE, wsl::Vector2i(), 'a', mod);
+        case SDLK_a: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'a', mod);
 
         case SDLK_b:
         case SDLK_KP_1:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(-1,1), 'b', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(-1,1), 'b', mod);
         }
         
-        case SDLK_c: return Input(Cmd::NONE, wsl::Vector2i(), 'c', mod);
+        case SDLK_c: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'c', mod);
 
         case SDLK_d:
         {
-            return Input(Cmd::OPEN_DROP, wsl::Vector2i(), 'd', mod);
+            return Input(Input::Cmd::OPEN_DROP, wsl::Vector2i(), 'd', mod);
         }
 
-        case SDLK_e: return Input(Cmd::NONE, wsl::Vector2i(), 'e', mod);
-        case SDLK_f: return Input(Cmd::NONE, wsl::Vector2i(), 'f', mod);
+        case SDLK_e: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'e', mod);
+        case SDLK_f: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'f', mod);
 
         case SDLK_g:
         {
-            return Input(Cmd::GET, wsl::Vector2i(), 'g', mod);
+            return Input(Input::Cmd::GET, wsl::Vector2i(), 'g', mod);
         }
 
         case SDLK_h:
         case SDLK_LEFT:
         case SDLK_KP_4:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(-1,0), 'h', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(-1,0), 'h', mod);
         }
         
         case SDLK_i:
         {
-            return Input(Cmd::OPEN_INV, wsl::Vector2i(), 'i', mod);
+            return Input(Input::Cmd::OPEN_INV, wsl::Vector2i(), 'i', mod);
         }
 
         case SDLK_j:
         case SDLK_DOWN:
         case SDLK_KP_2:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(0,1), 'j', mod); 
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(0,1), 'j', mod); 
         }
 
         case SDLK_k:
         case SDLK_UP:
         case SDLK_KP_8:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(0,-1), 'k', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(0,-1), 'k', mod);
         }
 
         case SDLK_l:
         {
             if(mod & KMOD_SHIFT)
             {
-                return Input(Cmd::LOOK, wsl::Vector2i(0,0), 'L', mod);
+                return Input(Input::Cmd::LOOK, wsl::Vector2i(0,0), 'L', mod);
             }
         }
         case SDLK_RIGHT:
         case SDLK_KP_6:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(1,0), 'l', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(1,0), 'l', mod);
         }
         
-        case SDLK_m: return Input(Cmd::NONE, wsl::Vector2i(), 'm', mod);
+        case SDLK_m: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'm', mod);
 
         case SDLK_n:
         case SDLK_KP_3:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(1,1), 'n', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(1,1), 'n', mod);
         }
 
-        case SDLK_o: return Input(Cmd::NONE, wsl::Vector2i(), 'o', mod);
-        case SDLK_p: return Input(Cmd::NONE, wsl::Vector2i(), 'p', mod);
+        case SDLK_o: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'o', mod);
+        case SDLK_p: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'p', mod);
         case SDLK_q:
         {
-            if(mod & KMOD_SHIFT)
+            if((mod & KMOD_SHIFT) || (mod & KMOD_CTRL))
             {
-                return Input(Cmd::QUIT, wsl::Vector2i(0,0), 'Q', mod);
+                return Input(Input::Cmd::QUIT, wsl::Vector2i(0,0), 'Q', mod);
             }
             else
             {
-                return Input(Cmd::NONE, wsl::Vector2i(), 'q', mod);
+                return Input(Input::Cmd::NONE, wsl::Vector2i(), 'q', mod);
             }
         }
-        case SDLK_r: return Input(Cmd::NONE, wsl::Vector2i(), 'r', mod);
+        case SDLK_r: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'r', mod);
 
-        case SDLK_s: return Input(Cmd::NONE, wsl::Vector2i(), 's', mod);
-        case SDLK_t: return Input(Cmd::NONE, wsl::Vector2i(), 't', mod);
+        case SDLK_s: return Input(Input::Cmd::NONE, wsl::Vector2i(), 's', mod);
+        case SDLK_t: return Input(Input::Cmd::NONE, wsl::Vector2i(), 't', mod);
 
         case SDLK_u:
         case SDLK_KP_9:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(1,-1), 'u', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(1,-1), 'u', mod);
         }
 
-        case SDLK_v: return Input(Cmd::NONE, wsl::Vector2i(), 'v', mod);
-        case SDLK_w: return Input(Cmd::NONE, wsl::Vector2i(), 'w', mod);
-        case SDLK_x: return Input(Cmd::NONE, wsl::Vector2i(), 'x', mod);
+        case SDLK_v: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'v', mod);
+        case SDLK_w: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'w', mod);
+        case SDLK_x: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'x', mod);
 
         case SDLK_y:
         case SDLK_KP_7:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(-1,-1), 'y', mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(-1,-1), 'y', mod);
         }
 
-        case SDLK_z: return Input(Cmd::NONE, wsl::Vector2i(), 'z', mod);
+        case SDLK_z: return Input(Input::Cmd::NONE, wsl::Vector2i(), 'z', mod);
 
         case SDLK_KP_5:
         case SDLK_KP_PERIOD:
         case SDLK_PERIOD:
         {
-            return Input(Cmd::MOVE, wsl::Vector2i(0,0), 0, mod);
+            return Input(Input::Cmd::MOVE, wsl::Vector2i(0,0), 0, mod);
         };
 
         case SDLK_ESCAPE:
         {
-            return Input(Cmd::ESCAPE, wsl::Vector2i(0,0), 0, mod);
+            return Input(Input::Cmd::ESCAPE, wsl::Vector2i(0,0), 0, mod);
         }
 
         case SDLK_RETURN:
         case SDLK_KP_ENTER:
         {
-            return Input(Cmd::ENTER, wsl::Vector2i(0,0), 0, mod);
+            return Input(Input::Cmd::ENTER, wsl::Vector2i(0,0), 0, mod);
         }
         case SDLK_F1:
         {
-            return Input(Cmd::FULLSCREEN);
+            return Input(Input::Cmd::FULLSCREEN);
         }
         default: { break; }
     }
 
-    return Input(Cmd::NONE);
+    return Input(Input::Cmd::NONE);
 }
